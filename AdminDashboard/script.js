6d99dffb66b13ceb8486a5ff17575f4db1a39ff4
Ali-Tharwat-181 <alitharwat358@gmail.com>
2025-04-28 22:49:01 +0300

    i updated the admin controller

--- AdminDashboard/script.js
+++ AdminDashboard/script.js
@@ -6,7 +6,8 @@ import {
 
 import handleCar from "../CarListings/JS/controller-instance.js";
 
-import { renderAllCars } from "../CarListings/JS/utils.js";
+import { InitializeStaticCars } from "./adminController.js";
+// import { renderAllCars } from "../CarListings/JS/utils.js";
 
 // DOM Elements
 const sidebarToggle = document.getElementById("sidebarToggle");
@@ -26,58 +27,10 @@ const bookingsTable = document
   .querySelector("tbody");
 const saveCarBtn = document.getElementById("saveCarBtn");
 const updateCarBtn = document.getElementById("updateCarBtn");
-const mainContainer = document.querySelector(".container #cardContainer");
 
-console.log(mainContainer);
 // Initialize handleCar
 
-if (handleCar.getAllCars().length === 0) {
-  const car1 = handleCar.createCar(
-    1,
-    "./Images/2.jpg",
-    "Toyota",
-    "Camry",
-    "Sedan",
-    30000,
-    "2022",
-    "A reliable sedan with a comfortable interior and advanced safety features."
-  );
-  const car2 = handleCar.createCar(
-    2,
-    "./Images/3.jpg",
-    "Honda",
-    "Civic",
-    "Sedan",
-    25000,
-    "2022",
-    "A compact car known for its reliability and fuel efficiency."
-  );
-  const car3 = handleCar.createCar(
-    3,
-    "./Images/4.jpg",
-    "Ford",
-    "Mustang",
-    "Coupe",
-    55000,
-    "2022",
-    "Sports car with a powerful engine and sleek design"
-  );
-  const car4 = handleCar.createCar(
-    4,
-    "./Images/5.jpg",
-    "Chevrolet",
-    "Impala",
-    "Sedan",
-    22000,
-    "2022",
-    "Spacious sedan with a comfortable interior."
-  );
-  handleCar.addCar(car1);
-  handleCar.addCar(car2);
-  handleCar.addCar(car3);
-  handleCar.addCar(car4);
-  handleCar.markCarAsBooked(2);
-}
+InitializeStaticCars(handleCar);
 
 // Sample data for bookings (in a real app, this would come from a database)
 const bookings = [
@@ -231,7 +184,6 @@ function setupEventListeners() {
         bookedCarsElement
       );
       renderCarsTable(handleCar, carsTable);
-      renderAllCars(handleCar.getAllCars(), mainContainer);
       // Show success message
       alert("Car added successfully!");
     } catch (error) {
