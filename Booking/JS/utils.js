cbe23f35a526c3a2e6625255372dacc5a0d3ec3d
OlaRedaAbdulrazeq <olaabdelrazeq@std.mans.edu.eg>
2025-05-02 03:17:20 +0300

    made a general functions that handle validation for different inputs and  visualized the validation on the form

--- Booking/JS/utils.js
+++ Booking/JS/utils.js
@@ -1,60 +1,64 @@
 import handleCar from "../../CarListings/JS/controller-instance.js";
 export function popupModal(id,data){
+  function goHome() {
+    window.location.href="../../home.html";
+  }
    let element = document.getElementById(id);
-   const emailParagraph = data.email.trim() !== ""? `<p>Email: ${data.email}</p>`: "";
    element.innerHTML=`
    <div class="modal  fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content align-items-center">
         <div class="modal-header">
           <h1 class="modal-title fs-5 text-center" id="exampleModalLabel">Booking Summary</h1>
-          <button type="button" class="btn-close justify-self-end" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <p>Full Name: ${data.firstName} ${data.lastName}</p>
-          <p>Phone Number: ${data.phoneNumber}</p>
-          ${emailParagraph}
-          <p>Booked Car: ${data.brand} ${data.model}</p>
-          <p>Pick Up Data: ${data.pickupLocation}, in ${data.pickupDate} at ${data.pickupTime}</p>
-          <p>Drop Off Data: ${data.dropoffLocation}, in ${data.dropoffDate} at ${data.DropoffTime}</p>
+          <p><strong>Full Name:</strong> ${data.firstName} ${data.lastName}</p>
+          <p><strong>Phone Number:</strong> ${data.phoneNumber}</p>
+          <p><strong>Email:</strong> ${data.email}</p>
+          <p><strong>Booked Car:</strong> ${data.brand} ${data.model}</p>
+          <p><strong>Pick Up Data:</strong> ${data.pickupLocation}, in ${data.pickupDate} at ${data.pickupTime}</p>
+          <p><strong>Drop Off Data:</strong> ${data.dropoffLocation}, in ${data.dropoffDate} at ${data.DropoffTime}</p>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="window.location.href="../../CarListings/carlist.html">Close</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id='modalCloseBtn' ">Close</button>
         </div>
       </div>
     </div>
   </div>`;
   const myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
     myModal.show();
+    document.getElementById('modalCloseBtn').addEventListener('click', () => {
+      window.location.href = "../../home.html";
+    });
 }
 
-export function generateCarForm() {    // This function shows the brand in the form of html .
-    const cars = handleCar.getCarsAvalible();  // Array of all available cars.
+// export function generateCarForm() {    // This function shows the brand in the form of html .
+//     const cars = handleCar.getCarsAvalible();  // Array of all available cars.
    
-    // Extract unique brands, models, and types using set and by using spread operator ==> convert set to an array.
-    const brands = [...new Set(cars.map(car => car.brand))];
-    const models = [...new Set(cars.map(car => car.model))];
+//     // Extract unique brands, models, and types using set and by using spread operator ==> convert set to an array.
+//     const brands = [...new Set(cars.map(car => car.brand))];
+//     const models = [...new Set(cars.map(car => car.model))];
   
-    //selects each select of brand , model .
-    const brandSelect = document.getElementById('brandSelect');
-    const modelSelect = document.getElementById('modelSelect');
+//     //selects each select of brand , model .
+//     const brandSelect = document.getElementById('brandSelect');
+//     const modelSelect = document.getElementById('modelSelect');
   
-    //Car brand dropdown
-    brands.forEach(brand => {
-      const option = document.createElement('option');
-      option.value = brand;
-      option.textContent = brand;
-      brandSelect.appendChild(option);
-    });
+//     //Car brand dropdown
+//     brands.forEach(brand => {
+//       const option = document.createElement('option');
+//       option.value = brand;
+//       option.textContent = brand;
+//       brandSelect.appendChild(option);
+//     });
   
-    // Car model dropdown
-    models.forEach(model => {
-      const option = document.createElement('option');
-      option.value = model;
-      option.textContent = model;
-      modelSelect.appendChild(option);
-    });
-  }
+//     // Car model dropdown
+//     models.forEach(model => {
+//       const option = document.createElement('option');
+//       option.value = model;
+//       option.textContent = model;
+//       modelSelect.appendChild(option);
+//     });
+//   }
   
  // BOOKING FUNCTIONALITY .
  export function navigateToBooking(car_id) {
--- home.js
+++ home.js
@@ -1,33 +1,48 @@
 import { controlDateView } from "./Booking/JS/controller.js";
-import { GeneralBooking } from "./Booking/JS/Model.js";
+import { validateLocation } from "./utils/validation.js";
 
-//show only dates from the current date.
-controlDateView();
 
-//getting values from GeneralBooking form present in the home page and scrolling  to carlist after submission  .
-const form = document.getElementById("generalBooking");
-// const findCar = document.getElementById("findCarbtn");
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  // const mainContainer = document.getElementById("mainCarContainer");
-  const pickUpLocation = document.getElementById("pickupL").value;
-  const dropOffLocation = document.getElementById("dropOffL").value
-  const pickUpDate = document.getElementById("pickUpdate").value;
-  const pickUpTime = document.getElementById("pickUptime").value;
-  const dropOffDate = document.getElementById("dropOffdate").value;
-  const dropOffTime = document.getElementById("dropOfftime").value;
-  let book = {
-    pickUpLocation:pickUpLocation,
-    pickUpDate:pickUpDate ,
-    pickUpTime:pickUpTime,
-    dropOffLocation:dropOffLocation,
-    dropOffDate:dropOffDate,
-    dropOffTime:dropOffTime
-  };
-  sessionStorage.setItem("GeneralBookingData", JSON.stringify(book));
-  window.location.href="CarListings/carlist.html";
-  // mainContainer.scrollIntoView({
-  //   behavior: "smooth", // Smooth scrolling
-  //   block: "start", // Align the section at the top of the page
-  // });
-});
+
+document.addEventListener('DOMContentLoaded',function(){
+  //show only dates from the current date.
+  controlDateView();
+  document.getElementById('pickupL').addEventListener("input",()=>validateLocation('pickupL','pickupL-error'));
+  document.getElementById("dropOffL").addEventListener("input",()=>validateLocation("dropOffL","dropOffL-error"));
+
+
+
+
+  const form = document.getElementById("generalBooking");
+    form.addEventListener("submit", function (event) {
+      const isPickupLocationValid = validateLocation('pickupL','pickupL-error');
+      const isDropoffLocationValid = validateLocation("dropOffL","dropOffL-error");
+      if (!(isPickupLocationValid &&isDropoffLocationValid )){
+           event.preventDefault();
+           return;
+      }else{
+        event.preventDefault(); // to be able to show the modal .
+        // const mainContainer = document.getElementById("mainCarContainer");
+        const pickUpLocation = document.getElementById("pickupL").value;
+        const dropOffLocation = document.getElementById("dropOffL").value
+        const pickUpDate = document.getElementById("pickUpdate").value;
+        const pickUpTime = document.getElementById("pickUptime").value;
+        const dropOffDate = document.getElementById("dropOffdate").value;
+        const dropOffTime = document.getElementById("dropOfftime").value;
+        let book = {
+          pickUpLocation:pickUpLocation,
+          pickUpDate:pickUpDate ,
+          pickUpTime:pickUpTime,
+          dropOffLocation:dropOffLocation,
+          dropOffDate:dropOffDate,
+          dropOffTime:dropOffTime
+        };
+        sessionStorage.setItem("GeneralBookingData", JSON.stringify(book));
+        window.location.href="CarListings/carlist.html";
+        // mainContainer.scrollIntoView({
+        //   behavior: "smooth", // Smooth scrolling
+        //   block: "start", // Align the section at the top of the page
+        // });
+      }
+ 
+    });
+  });
